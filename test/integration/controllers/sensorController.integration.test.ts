9817d3b9befa1d1ac010d62cf55da331b7670d09
Alix03 <alicebdf2@gmail.com>
2025-05-25 16:56:20 +0200

    feature: added integration test for sensorController

---
+++ test/integration/controllers/sensorController.integration.test.ts
@@ -0,0 +1,7 @@
+import * as sensorController from "@controllers/SensorController";
+import { SensorDAO } from "@dao/SensorDAO";
+import { SensorRepository } from "@repositories/SensorRepository";
+
+jest.mock("@repositories/SensorRepository");
+
+describe("SensorController integration", () => {});
